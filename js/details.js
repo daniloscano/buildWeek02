9f22b49c396faebb021ab47c0b6b84a545457392
Danilo Scano <daniloscano@gmail.com>
2025-03-07 20:33:43 +0100

    aggiunti dettagli pagina details; da finire layout

--- js/details.js
+++ js/details.js
@@ -5,8 +5,20 @@ const displayRoomDetails = () => {
     const details = rooms[pageId];
 
     const detailsTitle = document.querySelector('.details-title');
-
     detailsTitle.innerText = details.title;
+
+    const detailsImages = document.querySelectorAll('.details-images');
+    let imgCount = 0;
+    detailsImages.forEach(image => {
+        image.src = details.images[imgCount].src;
+        imgCount+=1;
+    });
+
+    const detailsType = document.querySelector('.room-type');
+    detailsType.innerText = details.roomType;
+
+    const detailsDesc = document.querySelector('.room-desc');
+    detailsDesc.innerText = details.roomDesc;
 };
 
 document.addEventListener('DOMContentLoaded', () => {
